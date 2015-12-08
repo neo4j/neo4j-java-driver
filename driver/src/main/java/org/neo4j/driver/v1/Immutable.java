72a092e4d12b4b578abb732ab36fcbda262990c5
Stefan Plantikow <stefan.plantikow@googlemail.com>
2015-12-08 13:14:26 +0100

    Update Immutable annotation to be documented

--- driver/src/main/java/org/neo4j/driver/v1/Immutable.java
+++ driver/src/main/java/org/neo4j/driver/v1/Immutable.java
@@ -18,12 +18,20 @@
  */
 package org.neo4j.driver.v1;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Indicates that instances of an annotated class are immutable
  */
 @Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Target( { ElementType.TYPE } )
 public @interface Immutable
 {
 }
