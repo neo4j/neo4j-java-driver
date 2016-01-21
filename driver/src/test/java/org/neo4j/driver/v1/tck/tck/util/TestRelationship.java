63978b23263e14733c2a1629b0a2c3cc877b4010
RagnarW <ragnar.wernersson@neotechnology.com>
2016-01-21 16:42:48 +0100

    Fixed headers

--- driver/src/test/java/org/neo4j/driver/v1/tck/tck/util/TestRelationship.java
+++ driver/src/test/java/org/neo4j/driver/v1/tck/tck/util/TestRelationship.java
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -79,9 +79,9 @@ public class TestRelationship implements Relationship
     }
 
     @Override
-    public Value value( String key )
+    public Value get( String key )
     {
-        return trv.asRelationship().value( key );
+        return trv.get( key );
     }
 
     @Override
