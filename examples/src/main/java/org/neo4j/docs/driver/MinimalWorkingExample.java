265294ca457ec15d0b4d5182cbb7b94af4cbaa78
Petra Selmer <petra.selmer@neotechnology.com>
2016-11-23 13:45:32 +0000

    Implemented `try-with-resources` in example

--- examples/src/main/java/org/neo4j/docs/driver/MinimalWorkingExample.java
+++ examples/src/main/java/org/neo4j/docs/driver/MinimalWorkingExample.java
@@ -20,9 +20,12 @@ package org.neo4j.docs.driver;
 
 // NOTE: Be careful about auto-formatting here: All imports should be between the tags below.
 // tag::minimal-example-import[]
-import org.neo4j.driver.v1.*;
-
-import static org.neo4j.driver.v1.Values.parameters;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
 // end::minimal-example-import[]
 
 public class MinimalWorkingExample
@@ -31,21 +34,21 @@ public class MinimalWorkingExample
     {
         // tag::minimal-example[]
         Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "neo4j" ) );
-        Session session = driver.session();
 
-        session.run( "CREATE (a:Person {name: {name}, title: {title}})",
-                parameters( "name", "Arthur", "title", "King" ) );
-
-        StatementResult result = session.run( "MATCH (a:Person) WHERE a.name = {name} " +
-                                              "RETURN a.name AS name, a.title AS title",
-                parameters( "name", "Arthur" ) );
-        while ( result.hasNext() )
+        try (Session session = driver.session())
         {
-            Record record = result.next();
-            System.out.println( record.get( "title" ).asString() + " " + record.get( "name" ).asString() );
-        }
 
-        session.close();
+            session.run( "CREATE (a:Person {name:'Arthur', title:'King'})" );
+
+            StatementResult result =
+                    session.run( "MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title" );
+            while ( result.hasNext() )
+            {
+                Record record = result.next();
+                System.out.println( record.get( "title" ).asString() + " " + record.get( "name" ).asString() );
+            }
+
+        }
         driver.close();
         // end::minimal-example[]
     }
