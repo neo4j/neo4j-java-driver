954febe76b301858d035f4d2dff73a95d44ceaab
Zhen <zhen.li@neotechnology.com>
2017-01-13 14:53:36 +0100

    Fixed the bug where if the client failed to establish connection with the server the server is not removed from the routing table

--- driver/src/test/java/org/neo4j/driver/internal/net/SocketClientTest.java
+++ driver/src/test/java/org/neo4j/driver/internal/net/SocketClientTest.java
@@ -22,31 +22,67 @@ import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.nio.ByteBuffer;
 import java.nio.channels.ByteChannel;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.neo4j.driver.internal.security.SecurityPlan;
 import org.neo4j.driver.v1.exceptions.ClientException;
 import org.neo4j.driver.v1.exceptions.ServiceUnavailableException;
 
+import static junit.framework.TestCase.fail;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.runners.Parameterized.Parameters;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.neo4j.driver.internal.logging.DevNullLogger.DEV_NULL_LOGGER;
 import static org.neo4j.driver.internal.net.BoltServerAddress.LOCAL_DEFAULT;
 
+@RunWith(Parameterized.class)
 public class SocketClientTest
 {
     private static final int CONNECTION_TIMEOUT = 42;
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
+
+    @Parameters(name = "{0} connections")
+    public static Collection<Object[]> data()
+    {
+        return Arrays.asList(new Object[][] {
+                { "insecure", SecurityPlan.insecure() },
+                { "encrypted", createEncryptedSecurityPlan() }
+        });
+    }
+    private static SecurityPlan createEncryptedSecurityPlan()
+    {
+        try
+        {
+            return SecurityPlan.forAllCertificates();
+        }
+        catch ( GeneralSecurityException | IOException e )
+        {
+            fail("Ensuring the creation of certs is not part of the focus of the test, while if failed to create a " +
+                 "cert, then this need to be fixed before running more tests on the top of this.");
+        }
+        return null;
+    }
+
+    private SecurityPlan plan;
+    public SocketClientTest( String testName, SecurityPlan plan )
+    {
+        this.plan = plan;
+    }
 
     // TODO: This is not possible with blocking NIO channels, unless we use inputStreams, but then we can't use
     // off-heap buffers. We need to swap to use selectors, which would allow us to time out.
@@ -58,8 +94,7 @@ public class SocketClientTest
         ServerSocket server = new ServerSocket( 0 );
         BoltServerAddress address = new BoltServerAddress( "localhost", server.getLocalPort() );
 
-        SecurityPlan securityPlan = SecurityPlan.insecure();
-        SocketClient client = new SocketClient( address, securityPlan, CONNECTION_TIMEOUT, DEV_NULL_LOGGER );
+        SocketClient client = dummyClient( address );
 
         // Expect
         exception.expect( ClientException.class );
@@ -69,9 +104,46 @@ public class SocketClientTest
         client.start();
     }
 
+    @Test
+    public void testConnectionTimeout() throws Throwable
+    {
+        BoltServerAddress address = new BoltServerAddress( "localhost", 1234 );
+
+        SocketClient client = dummyClient( address );
+
+        // Expect
+        exception.expect( ServiceUnavailableException.class );
+        exception.expectMessage( "Unable to connect to localhost:1234, " +
+                                 "ensure the database is running and that there is a working network connection to it." );
+
+        // When
+        client.start();
+    }
+
+    @Test
+    public void testIOExceptionWhenFailedToEstablishConnection() throws Throwable
+    {
+        BoltServerAddress address = new BoltServerAddress( "localhost", 0 ); // an illegal port
+
+        SecurityPlan securityPlan = SecurityPlan.insecure();
+        SocketClient client = new SocketClient( address, securityPlan, CONNECTION_TIMEOUT, DEV_NULL_LOGGER );
+
+        // Expect
+        exception.expect( ServiceUnavailableException.class );
+        exception.expectMessage( "Unable to process request: Can't assign requested address" );
+
+        // When
+        client.start();
+    }
+
+    private SocketClient dummyClient( BoltServerAddress address )
+    {
+        return new SocketClient( address, plan, CONNECTION_TIMEOUT, DEV_NULL_LOGGER );
+    }
+
     private SocketClient dummyClient()
     {
-        return new SocketClient( LOCAL_DEFAULT, SecurityPlan.insecure(), CONNECTION_TIMEOUT, DEV_NULL_LOGGER );
+        return dummyClient( LOCAL_DEFAULT );
     }
 
     @Test
