34378fc6ca643ca54a676e15c2e22334ab663839
lutovich <konstantin.lutovich@neotechnology.com>
2017-02-25 09:13:13 +0100

    Move IT to the correct package

--- driver/src/test/java/org/neo4j/driver/v1/integration/ConnectionHandlingIT.java
+++ driver/src/test/java/org/neo4j/driver/v1/integration/ConnectionHandlingIT.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.neo4j.driver.v1;
+package org.neo4j.driver.v1.integration;
 
 import org.junit.After;
 import org.junit.Before;
@@ -38,6 +38,16 @@ import org.neo4j.driver.internal.spi.ConnectionPool;
 import org.neo4j.driver.internal.spi.Connector;
 import org.neo4j.driver.internal.spi.PooledConnection;
 import org.neo4j.driver.internal.util.Clock;
+import org.neo4j.driver.v1.AuthToken;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Logging;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.StatementRunner;
+import org.neo4j.driver.v1.Transaction;
 import org.neo4j.driver.v1.exceptions.ClientException;
 import org.neo4j.driver.v1.summary.ResultSummary;
 import org.neo4j.driver.v1.util.TestNeo4j;
