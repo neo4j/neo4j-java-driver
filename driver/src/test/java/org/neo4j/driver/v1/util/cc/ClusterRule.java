19fc1e027e904ee1999f18fceeeb812d6f167051
lutovich <konstantin.lutovich@neotechnology.com>
2018-04-10 17:46:47 +0200

    Use same password for test single instance and cluster

--- driver/src/test/java/org/neo4j/driver/v1/util/cc/ClusterRule.java
+++ driver/src/test/java/org/neo4j/driver/v1/util/cc/ClusterRule.java
@@ -30,13 +30,14 @@ import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.util.Neo4jRunner;
 
 import static org.junit.Assume.assumeTrue;
+import static org.neo4j.driver.v1.util.Neo4jRunner.PASSWORD;
 import static org.neo4j.driver.v1.util.Neo4jRunner.TARGET_DIR;
+import static org.neo4j.driver.v1.util.Neo4jRunner.USER;
 import static org.neo4j.driver.v1.util.cc.CommandLineUtil.boltKitAvailable;
 
 public class ClusterRule extends ExternalResource
 {
     private static final Path CLUSTER_DIR = Paths.get( TARGET_DIR, "test-cluster" ).toAbsolutePath();
-    private static final String PASSWORD = "test";
     private static final int INITIAL_PORT = 20_000;
 
     public static final int CORE_COUNT = 3;
@@ -49,7 +50,7 @@ public class ClusterRule extends ExternalResource
 
     public AuthToken getDefaultAuthToken()
     {
-        return AuthTokens.basic( "neo4j", PASSWORD );
+        return AuthTokens.basic( USER, PASSWORD );
     }
 
     public static void stopSharedCluster()
