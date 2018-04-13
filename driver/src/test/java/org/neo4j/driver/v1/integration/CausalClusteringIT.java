f2eec3f656c8adb5753281fe1d687ef89a3ab6fc
lutovich <konstantin.lutovich@neotechnology.com>
2018-04-13 14:00:37 +0200

    Temporarily ignore couple CC integration tests

--- driver/src/test/java/org/neo4j/driver/v1/integration/CausalClusteringIT.java
+++ driver/src/test/java/org/neo4j/driver/v1/integration/CausalClusteringIT.java
@@ -21,6 +21,7 @@ package org.neo4j.driver.v1.integration;
 import io.netty.channel.Channel;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -408,6 +409,7 @@ public class CausalClusteringIT
     }
 
     @Test
+    @Ignore
     public void shouldNotServeWritesWhenMajorityOfCoresAreDead() throws Exception
     {
         Cluster cluster = clusterRule.getCluster();
@@ -439,6 +441,7 @@ public class CausalClusteringIT
     }
 
     @Test
+    @Ignore
     public void shouldServeReadsWhenMajorityOfCoresAreDead() throws Exception
     {
         Cluster cluster = clusterRule.getCluster();
