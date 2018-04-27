7c832fac25fb9f0810d00c1c007ff6b58afc1fe8
lutovich <konstantin.lutovich@neo4j.com>
2018-04-27 17:26:34 +0200

    Update license headers to "Neo4j Sweden AB"

--- driver/src/main/java/org/neo4j/driver/internal/metrics/InternalConnectionPoolMetrics.java
+++ driver/src/main/java/org/neo4j/driver/internal/metrics/InternalConnectionPoolMetrics.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2002-2018 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ * Copyright (c) 2002-2018 Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
  *
@@ -16,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.neo4j.driver.internal.metrics;
 
 import java.time.Duration;
