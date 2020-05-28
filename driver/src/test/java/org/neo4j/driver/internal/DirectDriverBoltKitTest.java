be4969838f664faaaf39bd2127462470f7acfa3a
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2020-05-28 14:03:35 +0100

    Upgrade Boltkit to 4.1. Adding BoltKit Test for NOOP message. (#703)

--- driver/src/test/java/org/neo4j/driver/internal/DirectDriverBoltKitTest.java
+++ driver/src/test/java/org/neo4j/driver/internal/DirectDriverBoltKitTest.java
@@ -544,6 +544,24 @@ class DirectDriverBoltKitTest
         }
     }
 
+    @Test
+    void shouldBeAbleHandleNOOPsDuringRunCypher() throws Exception
+    {
+        StubServer server = StubServer.start( "noop.script", 9001 );
+        URI uri = URI.create( "bolt://127.0.0.1:9001" );
+
+        try ( Driver driver = GraphDatabase.driver( uri, INSECURE_CONFIG ) )
+        {
+            try ( Session session = driver.session() )
+            {
+                List<String> names = session.run( "MATCH (n) RETURN n.name" ).list( rec -> rec.get( 0 ).asString() );
+                assertEquals( asList( "Foo", "Bar", "Baz" ), names );
+            }
+        }
+
+        assertThat( server.exitStatus(), equalTo( 0 ) );
+    }
+
     private static void testTxCloseErrorPropagation( String script, Consumer<Transaction> txAction, String expectedErrorMessage )
             throws Exception
     {
