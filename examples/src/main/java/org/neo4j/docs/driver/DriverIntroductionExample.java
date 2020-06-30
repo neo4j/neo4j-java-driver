b944effddd0805183c05cfdebe558c9e0cae5f46
Mark Needham <m.h.needham@gmail.com>
2020-06-30 10:59:29 +0100

    add aura example

---
+++ examples/src/main/java/org/neo4j/docs/driver/DriverIntroductionExample.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.docs.driver;
+
+// tag::driver-introduction-example-import[]
+
+import org.neo4j.driver.*;
+
+import static org.neo4j.driver.Values.parameters;
+// end::driver-introduction-example-import[]
+
+// tag::driver-introduction-example[]
+public class DriverIntroductionExample implements AutoCloseable
+{
+    private final Driver driver;
+
+    public DriverIntroductionExample(String uri, String user, String password )
+    {
+        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    }
+
+    @Override
+    public void close() throws Exception
+    {
+        driver.close();
+    }
+
+    public void printGreeting( final String message )
+    {
+        try ( Session session = driver.session() )
+        {
+            String greeting = session.writeTransaction( new TransactionWork<String>()
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    Result result = tx.run( "CREATE (a:Greeting) " +
+                                                     "SET a.message = $message " +
+                                                     "RETURN a.message + ', from node ' + id(a)",
+                            parameters( "message", message ) );
+                    return result.single().get( 0 ).asString();
+                }
+            } );
+            System.out.println( greeting );
+        }
+    }
+
+    public static void main( String... args ) throws Exception
+    {
+        try ( DriverIntroductionExample greeter = new DriverIntroductionExample( "bolt://localhost:7687", "neo4j", "password" ) )
+        {
+            greeter.printGreeting( "hello, world" );
+        }
+    }
+}
+// end::driver-introduction-example[]
+
+// tag::hello-world-output[]
+// hello, world, from node 1234
+// end::hello-world-output[]
