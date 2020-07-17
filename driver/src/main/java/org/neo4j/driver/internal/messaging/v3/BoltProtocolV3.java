cdac04afdb4e1abea940c420ce3912da4b12f12b
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2020-07-17 09:48:05 +0100

    Revert "Since Server v4.0 the server agent received in response to the Hello message is unreliable. Instead, since the server and Bolt protocol versions are aligned, we use the protocol version instead for all connections to Server 4.0 and higher (#708) (#718)" (#746)
    This reverts commit 31d3495a16aaf02a78d1ebfc70b0a73f0f554f46.

--- driver/src/main/java/org/neo4j/driver/internal/messaging/v3/BoltProtocolV3.java
+++ driver/src/main/java/org/neo4j/driver/internal/messaging/v3/BoltProtocolV3.java
@@ -80,7 +80,7 @@ public class BoltProtocolV3 implements BoltProtocol
         Channel channel = channelInitializedPromise.channel();
 
         HelloMessage message = new HelloMessage( userAgent, authToken );
-        HelloResponseHandler handler = new HelloResponseHandler( channelInitializedPromise, version() );
+        HelloResponseHandler handler = new HelloResponseHandler( channelInitializedPromise );
 
         messageDispatcher( channel ).enqueue( handler );
         channel.writeAndFlush( message, channel.voidPromise() );
