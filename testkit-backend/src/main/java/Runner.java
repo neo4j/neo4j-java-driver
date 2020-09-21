cecf1bba226ab7e2669eef2b1de17ff04994cf0e
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2020-09-21 16:02:27 +0100

    4.2 TestKit Backend (#761)
    This change introduces a testing component that allows for tests to be written once in a central repo (https://github.com/neo4j-drivers/testkit) and then executed for each driver.
    
    The build produces an executable server that acts as the intermediatory with TestKit.
    
    Co-authored-by: Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>

---
+++ testkit-backend/src/main/java/Runner.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.UncheckedIOException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Runner
+{
+    public static void main( String[] args ) throws IOException
+    {
+        ServerSocket serverSocket = new ServerSocket(9876);
+        System.out.println("Starting Java Testkit Backend Started");
+
+        while (true) {
+            System.out.println("Listening on socket");
+            Socket clientSocket = serverSocket.accept();
+
+            System.out.println( "Handling connection from: " + clientSocket.getRemoteSocketAddress() );
+            BufferedReader in = new BufferedReader( new InputStreamReader( clientSocket.getInputStream() ) );
+            BufferedWriter out = new BufferedWriter( new OutputStreamWriter( clientSocket.getOutputStream() ) );
+            CommandProcessor commandProcessor = new CommandProcessor(in, out);
+
+            boolean cont = true;
+            while (cont) {
+                try {
+                    cont = commandProcessor.process();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    clientSocket.close();
+                    cont = false;
+                }
+            }
+        }
+    }
+}
---
+++ testkit-backend/src/main/java/TestkitTypes.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.lang.reflect.Array;
+
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Value;
+import org.neo4j.driver.types.Node;
+import org.neo4j.driver.internal.value.IntegerValue;
+import org.neo4j.driver.internal.value.ListValue;
+import org.neo4j.driver.internal.value.MapValue;
+import org.neo4j.driver.internal.value.NodeValue;
+import org.neo4j.driver.internal.value.NullValue;
+import org.neo4j.driver.internal.value.StringValue;
+
+
+public class TestkitTypes
+{
+
+    public static String fromRecord( Record record )
+    {
+        String v = "";
+
+        for ( Value value : record.values() )
+        {
+            if (v != "") {
+                v += ",";
+            }
+            v += toTestkitType(value);
+        }
+
+        return v;
+    }
+
+
+    private static String toTestkitType( Object obj )
+    {
+        if ( obj instanceof Value) {
+            Value value = (Value) obj;
+            if ( value instanceof NullValue )
+            {
+                return "{\"data\" : null, \"name\" : \"CypherNull\"}";
+            } else if ( value instanceof IntegerValue )
+            {
+                return toTestkitType(value.asInt());
+            } else if ( value instanceof StringValue )
+            {
+                return toTestkitType(value.asString());
+            } else if ( value instanceof ListValue )
+            {
+                return toTestkitType(((ListValue)value).asList());
+            } else if ( value instanceof NodeValue )
+            {
+                Node node = ((NodeValue)value).asNode();
+                String v = String.format("{\"id\":%s,\"labels\":%s,\"props\":%s}",
+                    toTestkitType(node.id()), toTestkitType(node.labels()), toTestkitType(node.asMap()));
+                return Testkit.wrap("Node", v);
+            } else if ( value instanceof MapValue )
+            {
+                return "{\"data\" : {\"value\": {}, \"name\" : \"CypherMap\"}";
+            }
+        } else {
+            if (obj instanceof Integer || obj instanceof Long) {
+                return Testkit.wrap("CypherInt", Testkit.value(obj.toString()));
+            } else if (obj instanceof String) {
+                return Testkit.wrap("CypherString", Testkit.value("\""+obj.toString()+"\""));
+            } else if (obj instanceof List) {
+                List<?> list = (List<?>)obj;
+                String v = "";
+                for (int i = 0; i < list.size(); i++) {
+                    if (i > 0) {
+                        v += ",";
+                    }
+                    v += toTestkitType(list.get(i));
+                }
+                return Testkit.wrap("CypherList", Testkit.value("["+v+"]"));
+            } else if (obj instanceof Map) {
+                Map<String, ?> map = (Map<String, ?>)obj;
+                String v = "";
+
+                for (Map.Entry<String, ?> entry : map.entrySet()) {
+                    if (v != "") {
+                        v += ",";
+                    }
+                    v += String.format("\"%s\":%s", entry.getKey(), toTestkitType(entry.getValue()));
+                }
+                return Testkit.wrap("CypherMap", Testkit.value("{"+v+"}"));
+            }
+        }
+
+        throw new RuntimeException("Can not convert to testkit type:"+obj.getClass());
+    }
+
+    public static Object toDriver(JsonNode node)
+    {
+        String name = node.get("name").asText();
+        JsonNode data = node.get("data");
+        if (name.equals("CypherInt")) {
+            return data.get("value").asInt();
+        }
+        throw new RuntimeException("Can not convert from " + node + " to driver type");
+    }
+}
