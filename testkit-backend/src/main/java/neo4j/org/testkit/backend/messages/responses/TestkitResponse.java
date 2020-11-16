46c2e5acf70ffb4ed62d52e92f4e7a4f073d2263
Gregory Woods <gregory.woods@neo4j.com>
2020-11-16 10:04:27 +0000

    test-2

--- testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/responses/TestkitResponse.java
+++ testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/responses/TestkitResponse.java
@@ -23,5 +23,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="name")
 public interface TestkitResponse
 {
+
     String testkitName();
 }
