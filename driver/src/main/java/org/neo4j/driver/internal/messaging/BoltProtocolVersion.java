859a78483f00637b155c44bad8a0cd45130147e5
Antonio Barcélos <antonio.barcelos@neo4j.com>
2020-12-08 17:20:09 +0100

    Apply suggestions from code review
    Co-authored-by: Florent Biville <445792+fbiville@users.noreply.github.com>

--- driver/src/main/java/org/neo4j/driver/internal/messaging/BoltProtocolVersion.java
+++ driver/src/main/java/org/neo4j/driver/internal/messaging/BoltProtocolVersion.java
@@ -34,7 +34,7 @@ public class BoltProtocolVersion implements Comparable<BoltProtocolVersion>
     public static BoltProtocolVersion fromRawBytes( int rawVersion )
     {
         int major = rawVersion & 0x000000FF;
-        int minor = ( rawVersion >> 8 ) & 0x000000FF;
+        int minor = (rawVersion >> 8) & 0x000000FF;
 
         return new BoltProtocolVersion( major, minor );
     }
@@ -55,15 +55,15 @@ public class BoltProtocolVersion implements Comparable<BoltProtocolVersion>
         return shiftedMinor | majorVersion;
     }
 
-    public int toIntRange(BoltProtocolVersion minVersion)
+    public int toIntRange( BoltProtocolVersion minVersion )
     {
-        if(majorVersion != minVersion.majorVersion)
+        if ( majorVersion != minVersion.majorVersion )
         {
-            throw new IllegalArgumentException( "Versions should be from the same version" );
+            throw new IllegalArgumentException( "Versions should be from the same major version" );
         }
-        else if (minorVersion < minVersion.minorVersion)
+        else if ( minorVersion < minVersion.minorVersion )
         {
-            throw new IllegalArgumentException("Max version should be newest than min version");
+            throw new IllegalArgumentException( "Max version should be newer than min version" );
         }
         int range = minorVersion - minVersion.minorVersion;
         int shiftedRange = range << 16;
