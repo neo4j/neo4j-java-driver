720b88f46a0f3ba75627c296e55d27b8372226e3
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2021-01-08 15:26:51 +0100

    Stress startable from testkit
    Needed check on encryption on/off in test code by probing the scheme.

--- testkit/build.py
+++ testkit/build.py
@@ -2,12 +2,13 @@
 Executed in java driver container.
 Responsible for building driver and test backend.
 """
-import os, subprocess
+import subprocess
+
 
 def run(args):
     subprocess.run(
         args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
 
+
 if __name__ == "__main__":
     run(["mvn", "clean", "install", "-P", "!determine-revision", "-DskipTests"])
-
--- testkit/unittests.py
+++ testkit/unittests.py
@@ -3,12 +3,13 @@ Executed in Java driver container.
 Responsible for running unit tests.
 Assumes driver has been setup by build script prior to this.
 """
+import subprocess
 
-import os, subprocess
 
 def run(args):
     subprocess.run(
         args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
+
 
 if __name__ == "__main__":
     run(["mvn", "test", "-Dmaven.gitcommitid.skip"])
