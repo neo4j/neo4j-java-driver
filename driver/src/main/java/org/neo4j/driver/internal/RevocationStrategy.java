273e1ada6cd17a671e76ec82985912a2b39a9216
Johannes Donath <johannes.donath@neotechnology.com>
2021-02-17 09:39:01 +0100

    Removed the year component from all copyright notices.

--- driver/src/main/java/org/neo4j/driver/internal/RevocationStrategy.java
+++ driver/src/main/java/org/neo4j/driver/internal/RevocationStrategy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2020 "Neo4j,"
+ * Copyright (c) "Neo4j"
  * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.neo4j.driver.internal;
 
 public enum RevocationStrategy
