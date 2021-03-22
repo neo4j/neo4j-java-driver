7ca9aba1d0ad0e02a165ff456ae29635e64ce8e3
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2021-03-22 13:39:35 +0000

    Imported testkit directory

---
+++ testkit/build.py
@@ -0,0 +1,14 @@
+"""
+Executed in java driver container.
+Responsible for building driver and test backend.
+"""
+import subprocess
+
+
+def run(args):
+    subprocess.run(
+        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
+
+
+if __name__ == "__main__":
+    run(["mvn", "clean", "install", "-P", "!determine-revision", "-DskipTests"])
---
+++ testkit/unittests.py
@@ -0,0 +1,15 @@
+"""
+Executed in Java driver container.
+Responsible for running unit tests.
+Assumes driver has been setup by build script prior to this.
+"""
+import subprocess
+
+
+def run(args):
+    subprocess.run(
+        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
+
+
+if __name__ == "__main__":
+    run(["mvn", "test", "-Dmaven.gitcommitid.skip"])
