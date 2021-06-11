bea0f9b27906abcea20e59baf4ebb370d2c2219c
Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
2021-06-11 13:54:33 +0100

    Add support for BackendError to backend responses (#914)

--- testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/responses/BackendError.java
+++ testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/responses/BackendError.java
@@ -18,7 +18,6 @@
  */
 package neo4j.org.testkit.backend.messages.responses;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,14 +25,21 @@ import lombok.Setter;
 @Setter
 @Getter
 @Builder
-public class TestkitErrorResponse implements TestkitResponse
+public class BackendError implements TestkitResponse
 {
-    @JsonProperty("msg")
-    private String errorMessage;
+    private final BackendErrorBody data;
 
     @Override
     public String testkitName()
     {
-        return "Error";
+        return "BackendError";
+    }
+
+    @Setter
+    @Getter
+    @Builder
+    public static class BackendErrorBody
+    {
+        private String msg;
     }
 }
