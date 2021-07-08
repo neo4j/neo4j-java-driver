59921d2710ed9f3123a4a6fb0b0ae26d44a0a6be
Robsdedude <dev@rouvenbauer.de>
2021-07-08 16:40:13 +0200

    Skip test test_disconnect_on_hello (#957)
    The test requires bolt version 4.3

--- testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/StartTest.java
+++ testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/StartTest.java
@@ -52,6 +52,7 @@ public class StartTest implements TestkitRequest
                 .put( "^.+routing.Routing.*\\.test_should_successfully_get_server_protocol_version$", "The test is not applicable to 4.2 driver" );
         SKIP_PATTERN_TO_REASON.put( "^.+routing.Routing.*\\.test_should_successfully_get_server_agent$", "The test is not applicable to 4.2 driver" );
         SKIP_PATTERN_TO_REASON.put( "^.+disconnects.TestDisconnects.test_client_says_goodbye$", "This test uses 4.3 Bolt" );
+        SKIP_PATTERN_TO_REASON.put( "^.+disconnects.TestDisconnects.test_disconnect_on_hello", "This test uses 4.3 Bolt" );
         SKIP_PATTERN_TO_REASON.put( "^.+disconnects.TestDisconnects.test_disconnect_after_hello", "This test uses 4.3 Bolt" );
         SKIP_PATTERN_TO_REASON.put( "^.+disconnects.TestDisconnects.test_disconnect_on_tx_begin", "The 4.2 driver disconnects after first next" );
         SKIP_PATTERN_TO_REASON.put( "^.+disconnects.TestDisconnects.test_disconnect_on_tx_run", "The 4.2 driver disconnects after first next" );
