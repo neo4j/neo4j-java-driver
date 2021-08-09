42c2105c4fc160f4ed808c834dbacb67e98cea18
Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
2021-08-09 18:06:39 +0100

    Migrate tests to Testkit and exclude routing context when it is null (#974)
    Migrated tests:
    - shouldBeAbleRunCypher -> test_should_read_successfully_using_write_session_run
    - shouldSendMultipleBookmarks -> test_send_and_receive_multiple_bookmarks_write_tx
    - shouldSendNullRoutingContextForBoltUri -> test_should_exclude_routing_context (optimization)
    - shouldLogConnectionIdInDebugMode -> not portable to Testkit, logic covered by unit tests:
      - InboundMessageDispatcherTest.shouldCreateChannelActivityLoggerAndLogDebugMessageOnMessageHandling
      - InboundMessageDispatcherTest.shouldCreateChannelActivityLoggerAndLogDebugMessageOnChannelError
      - ChannelActivityLoggerTest.* (formatted logging)
    - shouldSendReadAccessModeInQueryMetadata -> test_should_read_successfully_using_read_session_run
    - shouldNotSendWriteAccessModeInQueryMetadata -> test_should_read_successfully_using_write_session_run
    - shouldSendCustomerUserAgentInHelloMessage -> test_should_send_custom_user_agent_using_write_session_run

--- driver/src/main/java/org/neo4j/driver/internal/async/inbound/InboundMessageDispatcher.java
+++ driver/src/main/java/org/neo4j/driver/internal/async/inbound/InboundMessageDispatcher.java
@@ -281,4 +281,10 @@ public class InboundMessageDispatcher implements ResponseMessageHandler
 
         void run( MessageType messageType );
     }
+
+    //    For testing only
+    Logger getLog()
+    {
+        return log;
+    }
 }
