a5f3ab121b6c98cb05655bf6e3c0f94ee8792476
Robsdedude <rouven.bauer@neo4j.com>
2022-01-24 19:44:10 +0100

    Add Result.peek support to TestKit back end (#1110)

--- testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/TestkitRequest.java
+++ testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/TestkitRequest.java
@@ -40,7 +40,8 @@ import java.util.concurrent.CompletionStage;
         @JsonSubTypes.Type( DomainNameResolutionCompleted.class ), @JsonSubTypes.Type( StartTest.class ),
         @JsonSubTypes.Type( TransactionRollback.class ), @JsonSubTypes.Type( GetFeatures.class ),
         @JsonSubTypes.Type( GetRoutingTable.class ), @JsonSubTypes.Type( TransactionClose.class ),
-        @JsonSubTypes.Type( ResultList.class ), @JsonSubTypes.Type( GetConnectionPoolMetrics.class )
+        @JsonSubTypes.Type( ResultList.class ), @JsonSubTypes.Type( GetConnectionPoolMetrics.class ),
+        @JsonSubTypes.Type( ResultPeek.class )
 } )
 public interface TestkitRequest
 {
