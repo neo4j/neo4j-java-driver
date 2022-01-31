ae6a3c3fc0e04f62ed587a83a1869cc685dacae4
injectives <11927660+injectives@users.noreply.github.com>
2022-01-31 23:59:07 +0200

    TestKit backend output goes to stdout/stderr (#1084) (#1138)
    From there, TestKit can pick it up and write it into logfiles if desired.
    https://github.com/neo4j-drivers/testkit/pull/309
    
    Co-authored-by: Robsdedude <rouven.bauer@neo4j.com>

--- testkit/backend.py
+++ testkit/backend.py
@@ -3,12 +3,12 @@ Executed in Java driver container.
 Assumes driver and backend has been built.
 Responsible for starting the test backend.
 """
-import os, subprocess
+import os
+import subprocess
+import sys
 
 
 if __name__ == "__main__":
-    err = open("/artifacts/backenderr.log", "w")
-    out = open("/artifacts/backendout.log", "w")
     subprocess.check_call(
-        ["java", "-jar", "testkit-backend/target/testkit-backend.jar"], stdout=out, stderr=err)
+        ["java", "-jar", "testkit-backend/target/testkit-backend.jar"], stdout=sys.stdout, stderr=sys.stderr)
 
