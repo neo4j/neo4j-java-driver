39edde629d3a114518c8be4fbf6b9d20516df965
injectives <11927660+injectives@users.noreply.github.com>
2022-05-06 21:15:07 +0100

    Skip test_trusted_ca_correct_hostname test (#1213)

--- testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/StartTest.java
+++ testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/StartTest.java
@@ -74,6 +74,8 @@ public class StartTest implements TestkitRequest {
                 "^.*\\.TestOptimizations\\.test_uses_implicit_default_arguments_multi_query$", skipMessage);
         COMMON_SKIP_PATTERN_TO_REASON.put(
                 "^.*\\.TestOptimizations\\.test_uses_implicit_default_arguments_multi_query_nested$", skipMessage);
+        skipMessage = "This test became flaky and needs investigation";
+        COMMON_SKIP_PATTERN_TO_REASON.put("^.*\\.test_trusted_ca_correct_hostname$", skipMessage);
 
         ASYNC_SKIP_PATTERN_TO_REASON.putAll(COMMON_SKIP_PATTERN_TO_REASON);
         ASYNC_SKIP_PATTERN_TO_REASON.put(
