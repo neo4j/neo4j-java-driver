12e5d34c7609d5ab4e331f7bceb19654818467d8
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2022-07-11 10:15:21 +0100

    Enable TLSv1.1 for Testkit testing

--- testkit/backend.py
+++ testkit/backend.py
@@ -10,7 +10,7 @@ import sys
 
 if __name__ == "__main__":
     subprocess.check_call(
-        ["java", "-Djdk.tls.client.protocols=TLSv1.3,TLSv1.2,TLSv1.1", "-jar", "testkit-backend/target/testkit-backend.jar",
+        ["java", "-Djava.security.properties=/testkit.java.security", "-jar", "testkit-backend/target/testkit-backend.jar",
         os.getenv('TEST_BACKEND_SERVER', '')],
         stdout=sys.stdout, stderr=sys.stderr
     )
