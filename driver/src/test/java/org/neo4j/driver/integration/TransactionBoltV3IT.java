0bfa504fca2b11999c92bcec0f98a6ce6f149eae
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2022-09-07 16:20:42 +0100

    Enable UTC tests for Bolt 5.0

--- driver/src/test/java/org/neo4j/driver/integration/TransactionBoltV3IT.java
+++ driver/src/test/java/org/neo4j/driver/integration/TransactionBoltV3IT.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.neo4j.driver.internal.util.Neo4jFeature.BOLT_V3;
-import static org.neo4j.driver.internal.util.Neo4jFeature.BOLT_V5;
 import static org.neo4j.driver.testutil.TestUtil.TX_TIMEOUT_TEST_TIMEOUT;
 import static org.neo4j.driver.testutil.TestUtil.await;
 
@@ -47,7 +46,6 @@ import org.neo4j.driver.async.AsyncTransaction;
 import org.neo4j.driver.async.ResultCursor;
 import org.neo4j.driver.exceptions.ClientException;
 import org.neo4j.driver.exceptions.TransientException;
-import org.neo4j.driver.internal.util.DisabledOnNeo4jWith;
 import org.neo4j.driver.internal.util.EnabledOnNeo4jWith;
 import org.neo4j.driver.testutil.DriverExtension;
 import org.neo4j.driver.testutil.ParallelizableIT;
@@ -85,7 +83,6 @@ class TransactionBoltV3IT {
     }
 
     @Test
-    @DisabledOnNeo4jWith(BOLT_V5)
     void shouldSetTransactionMetadataAsync() {
         Map<String, Object> metadata = new HashMap<>();
         metadata.put("hello", "world");
