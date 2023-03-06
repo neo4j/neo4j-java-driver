c0737be631d146e3ede8acb4dce528aa72697b5c
Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
2023-03-06 13:00:33 +0000

    Make the shaded artifact compile as a module (#1388)
    * Make the shaded artifact compile as a module
    
    This update makes sure the shaded artifact is compiled as a module. This is needed for the features that depend on that, for instance the `sealed` types used across packages.
    
    In addition, the shaded `module-info.java` is kept as a source file so that it could be checked by the licensing plugin and also be added into the sources jar.
    
    * Update README

---
+++ bundle/src/main/jpms/module-info.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * The Neo4j Java Driver module.
+ */
+@SuppressWarnings({"requires-automatic", "requires-transitive-automatic"})
+module org.neo4j.driver {
+    exports org.neo4j.driver;
+    exports org.neo4j.driver.async;
+    exports org.neo4j.driver.reactive;
+    exports org.neo4j.driver.reactivestreams;
+    exports org.neo4j.driver.types;
+    exports org.neo4j.driver.summary;
+    exports org.neo4j.driver.net;
+    exports org.neo4j.driver.util;
+    exports org.neo4j.driver.exceptions;
+
+    requires transitive java.logging;
+    requires transitive org.reactivestreams;
+    requires static micrometer.core;
+    requires static org.graalvm.sdk;
+    requires static org.slf4j;
+    requires static java.management;
+}
