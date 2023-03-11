bf4d017474d60251d1a14b8f8bf6a307c02710f5
Antonio Barcélos <antonio.barcelos@neo4j.com>
2023-03-11 08:15:38 -0300

    Fix skipped test name (#1391)
    * Fix skipped test name
    
    The test `stub.authorization.test_authorization.TestAuthenticationSchemes.test_custom_scheme_empty` was
    rename to `stub.authorization.test_authorization.TestAuthenticationSchemesV4x4.test_custom_scheme_empty` in
    the PR https://github.com/neo4j-drivers/testkit/pull/542.
    This change is causing the pipeline fails for java.
    
    * Update testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/StartTest.java
    
    Co-authored-by: Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>

--- testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/StartTest.java
+++ testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/StartTest.java
@@ -73,6 +73,8 @@ public class StartTest implements TestkitRequest {
                 skipMessage);
         skipMessage = "This test needs updating to implement expected behaviour";
         COMMON_SKIP_PATTERN_TO_REASON.put("^.*\\.TestAuthenticationSchemes\\.test_custom_scheme_empty$", skipMessage);
+        COMMON_SKIP_PATTERN_TO_REASON.put(
+                "^.*\\.TestAuthenticationSchemes[^.]+\\.test_custom_scheme_empty$", skipMessage);
         skipMessage = "Driver does not implement optimization for qid in explicit transaction";
         COMMON_SKIP_PATTERN_TO_REASON.put(
                 "^.*\\.TestOptimizations\\.test_uses_implicit_default_arguments$", skipMessage);
