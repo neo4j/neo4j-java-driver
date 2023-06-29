52aef12ce1b6c43190b9ddef4c0e83e1972afebd
Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
2023-06-29 10:07:23 +0100

    Notify handlers of a failure on graceful pool closure (#1442)

--- driver/src/main/java/org/neo4j/driver/internal/async/inbound/InboundMessageDispatcher.java
+++ driver/src/main/java/org/neo4j/driver/internal/async/inbound/InboundMessageDispatcher.java
@@ -168,6 +168,10 @@ public class InboundMessageDispatcher implements ResponseMessageHandler {
         if (!gracefullyClosed) {
             handleChannelError(cause);
         } else {
+            while (!handlers.isEmpty()) {
+                ResponseHandler handler = removeHandler();
+                handler.onFailure(cause);
+            }
             channel.close();
         }
     }
