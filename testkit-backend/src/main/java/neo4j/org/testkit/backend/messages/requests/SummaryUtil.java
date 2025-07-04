891ca3fe18ab625d896b22f5a9a1fb7fd6377d64
Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
2025-07-04 11:20:56 +0100

    feat(gql-status-object): Introduce GqlNotification (#1667)
    BREAKING CHANGE: this update separates the new GQL Status Object hierarchy from the legacy `Notification` type by introducing a new `GqlNotification` interface that extends the `GqlStatusObject`.
    
    The update is based on the feedback received on the GQL Status Object feature, which is currently in preview. This should make it easier to understand the new GQL Status Objects and migrate from using the legacy `Notification` to the new types.
    
    Eventually, the new hierarchy is expected to replace the legacy `Notification` that is expected to be deprecated and phased out at a later date.

--- testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/SummaryUtil.java
+++ testkit-backend/src/main/java/neo4j/org/testkit/backend/messages/requests/SummaryUtil.java
@@ -24,8 +24,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import neo4j.org.testkit.backend.messages.responses.Summary;
 import org.neo4j.driver.internal.InternalNotificationSeverity;
+import org.neo4j.driver.summary.GqlNotification;
 import org.neo4j.driver.summary.InputPosition;
-import org.neo4j.driver.summary.Notification;
 import org.neo4j.driver.summary.Plan;
 import org.neo4j.driver.summary.ProfiledPlan;
 import org.neo4j.driver.summary.QueryType;
@@ -82,15 +82,16 @@ public class SummaryUtil {
                             .gqlStatus(gqlStatusObject.gqlStatus())
                             .statusDescription(gqlStatusObject.statusDescription())
                             .diagnosticRecord(gqlStatusObject.diagnosticRecord());
-                    if (gqlStatusObject instanceof Notification notification) {
-                        builder = builder.position(toInputPosition(notification.position()))
+                    if (gqlStatusObject instanceof GqlNotification notification) {
+                        builder = builder.position(
+                                        toInputPosition(notification.position().orElse(null)))
                                 .severity(notification
-                                        .severityLevel()
+                                        .severity()
                                         .map(InternalNotificationSeverity.class::cast)
                                         .map(InternalNotificationSeverity::type)
                                         .map(InternalNotificationSeverity.Type::toString)
                                         .orElse("UNKNOWN"))
-                                .rawSeverity(notification.rawSeverityLevel().orElse(null))
+                                .rawSeverity(notification.rawSeverity().orElse(null))
                                 .classification(notification
                                         .classification()
                                         .map(Objects::toString)
