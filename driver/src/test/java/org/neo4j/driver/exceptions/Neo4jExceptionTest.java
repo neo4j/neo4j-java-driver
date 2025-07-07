ceb94cae943e840d9088533441273d476cc48973
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2025-07-07 23:44:30 +0100

    feat(gql-error): add GQLSTATUS finders
    In some situations it might be needed to check if GQL error or any error in its GQL error chain has a specific GQLSTATUS.
    
    This update adds 2 new methods to `Neo4jException` to make it easier:
    - `boolean containsGqlStatus(String gqlStatus)`
    - `Optional<Neo4jException> findByGqlStatus(String gqlStatus)`

--- driver/src/test/java/org/neo4j/driver/exceptions/Neo4jExceptionTest.java
+++ driver/src/test/java/org/neo4j/driver/exceptions/Neo4jExceptionTest.java
@@ -17,6 +17,7 @@
 package org.neo4j.driver.exceptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import java.util.Map;
@@ -82,6 +83,71 @@ class Neo4jExceptionTest {
         assertError(exception, exception1, exception1);
         assertError(exception1, exception2, exception2);
         assertError(exception2, exception3, null);
+    }
+
+    @ParameterizedTest
+    @MethodSource("gqlErrorsArgs")
+    void shouldFindByGqlStatus(Neo4jException exception, int statusIndex, boolean shouldBeFound) {
+        // given
+        var status = "status" + statusIndex;
+
+        // when
+        var exceptionWithStatus = exception.findByGqlStatus(status);
+
+        // then
+        if (shouldBeFound) {
+            assertTrue(exceptionWithStatus.isPresent());
+            assertEquals(getByIndex(exception, statusIndex), exceptionWithStatus.get());
+        } else {
+            assertTrue(exceptionWithStatus.isEmpty());
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("gqlErrorsArgs")
+    void shouldReturnIfErrorContainsGqlStatus(Neo4jException exception, int statusIndex, boolean shouldBeFound) {
+        // given
+        var status = "status" + statusIndex;
+
+        // when
+        var contains = exception.containsGqlStatus(status);
+
+        // then
+        assertEquals(shouldBeFound, contains);
+    }
+
+    static Stream<Arguments> gqlErrorsArgs() {
+        return Stream.of(
+                Arguments.of(buildGqlErrors(1, 1), 0, true),
+                Arguments.of(buildGqlErrors(1, 1), -1, false),
+                Arguments.of(buildGqlErrors(20, 20), 0, true),
+                Arguments.of(buildGqlErrors(20, 20), 10, true),
+                Arguments.of(buildGqlErrors(20, 20), 19, true),
+                Arguments.of(buildGqlErrors(20, 20), -1, false),
+                Arguments.of(buildGqlErrors(20, 15), 0, true),
+                Arguments.of(buildGqlErrors(20, 15), 14, true),
+                Arguments.of(buildGqlErrors(20, 15), -1, false));
+    }
+
+    @SuppressWarnings("DataFlowIssue")
+    static Neo4jException buildGqlErrors(int totalSize, int gqlErrorsSize) {
+        Exception exception = null;
+        for (var i = totalSize - 1; i >= gqlErrorsSize; i--) {
+            exception = new IllegalStateException("illegal state" + i, exception);
+        }
+        for (var i = gqlErrorsSize - 1; i >= 0; i--) {
+            exception = new Neo4jException(
+                    "status" + i, "description" + i, "code", "message", Collections.emptyMap(), exception);
+        }
+        return (Neo4jException) exception;
+    }
+
+    static Throwable getByIndex(Throwable exception, int depth) {
+        var result = exception;
+        for (var i = 0; i < depth; i++) {
+            result = result.getCause();
+        }
+        return result;
     }
 
     private void assertError(Neo4jException exception, Throwable expectedCause, Neo4jException expectedGqlCause) {
